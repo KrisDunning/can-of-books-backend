a660a07a9bb67ba653cbff1094282b5ea3d1042b
Kris Dunning <dunning.kris@gmail.com>
2022-06-27 14:44:08 -0700

    pushing for mongo atlas connection test

--- server.js
+++ server.js
@@ -3,16 +3,34 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3001;
+const mongoose= require('mongoose');
+// connect Mongoose to our MongoDB
+mongoose.connect(process.env.MONGOURL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+app.get('/', (request,response)=>{
+  response.status(200).send('Connected to Heroku Can of Books Home Page');
+};
 
 app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
+  
+  response.status(200).send('test request received')
+  
 })
+
+app.get('*'),(request,response)=>{
+  response.status(404).send('Sorry Books Not Found! Might I reccomend the local library.');
+};
+
+
+// add validation to confirm we are wired up to our mongo DB
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
